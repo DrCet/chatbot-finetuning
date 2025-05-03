b6a7a398a20ed6433c449808f79e3fcdef08c980
DrCet <cetdocteurhehe@gmail.com>
2025-05-03 23:12:36 +0700

    T

--- run_imagecap_finetuning.py
+++ run_imagecap_finetuning.py
@@ -229,8 +229,7 @@ def main():
             data_args.dataset_name,
             data_args.dataset_config_name,
             split=data_args.train_split_name,
-            cache_dir=model_args.cache_dir,
-            token=model_args.token
+            cache_dir=model_args.cache_dir
         )
     if training_args.do_eval and data_args.eval_split_name is not None:
         raw_datasets['validation'] = load_dataset(
@@ -254,17 +253,12 @@ def main():
     config = AutoConfig.from_pretrained(
         model_args.config_name_or_path if model_args.config_name_or_path else model_args.model_name_or_path,
         cache_dir=model_args.cache_dir,
-        revision=model_args.model_revision,
-        token=model_args.token,
-        trust_remote_code=model_args.trust_remote_code,
     )
 
     processor = AutoProcessor.from_pretrained(
         model_args.model_name_or_path if model_args.model_name_or_path else model_args.config_name_or_path,
         cache_dir=model_args.cache_dir,
-        revision=model_args.model_revision,
-        token=model_args.token,
-        trust_remote_code=model_args.trust_remote_code,
+
     )
 
 
@@ -320,7 +314,7 @@ def main():
     data_collator = DataCollatorWithPadding(
         processor=processor
     ) 
-    
+
     # 10. Training and evaluation
     trainer = Trainer(
         model=model,
